62286bfa53b3f8453e1d6e9c4a76f3610d44ee9c
Gabi Hoffmann <hoffmann.agf@gmail.com>
2024-01-22 08:54:50 -0300

    feat: more tests using useCallback

--- src/CallbackHook/index.jsx
+++ src/CallbackHook/index.jsx
@@ -2,23 +2,34 @@ import { useCallback, useState } from "react";
 
 const set = new Set();
 const setCallback = new Set();
+const setCallbackUp = new Set();
 export function CallbackHook() {
   const [counter, setCounter] = useState(0);
   const [counterCallback, setCounterCallback] = useState(0);
+  const [dependenceCallback, setDependenceCallback] = useState(0);
 
   function handleSetCounter() {
     setCounter((prev) => ++prev);
     set.add(handleSetCounter);
   }
 
-  console.log("Set: ", set);
+  console.log("Set function: ", set);
 
   const handleSetCounterCallback = useCallback(() => {
     setCounterCallback((prev) => ++prev);
     setCallback.add(handleSetCounterCallback);
   }, []);
 
-  console.log("Set callback: ", setCallback);
+  console.log("Set callback:", setCallback);
+
+  const handleSetCounterCallbackUp = useCallback(() => {
+    setCounterCallback((prev) => ++prev);
+    alert("dependence:", dependenceCallback);
+    setCallbackUp.add(handleSetCounterCallbackUp);
+  }, [dependenceCallback]);
+
+  console.log("Set callback with dependence: ", setCallbackUp);
+
   return (
     <div>
       Contador: {counter}
@@ -26,6 +37,11 @@ export function CallbackHook() {
       <br />
       Callback contador: {counterCallback}
       <button onClick={handleSetCounterCallback}>Plus</button>
+      <br />
+      <button onClick={handleSetCounterCallbackUp}>Plus with dependence</button>
+      <button onClick={() => setDependenceCallback((prev) => ++prev)}>
+        change dependence
+      </button>
     </div>
   );
 }
